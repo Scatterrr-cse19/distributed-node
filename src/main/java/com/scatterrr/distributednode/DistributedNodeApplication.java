b17b8236fdc5f7ff7affb67872daec85fc241f38
BanulaKumarage <banula.19@cse.mrt.ac.lk>
2024-04-09 15:09:01 +0530

    Initial Folder Structure

---
+++ src/main/java/com/scatterrr/distributednode/DistributedNodeApplication.java
@@ -0,0 +1,13 @@
+package com.scatterrr.distributednode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedNodeApplication.class, args);
+	}
+
+}
