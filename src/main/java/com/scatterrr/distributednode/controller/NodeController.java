9d02faed14a0210e660868ed0064db4036d18595
BanulaKumarage <banula.19@cse.mrt.ac.lk>
2024-04-25 23:45:01 +0530

    Added the chunks upload endpoint

--- src/main/java/com/scatterrr/distributednode/controller/NodeController.java
+++ src/main/java/com/scatterrr/distributednode/controller/NodeController.java
@@ -4,10 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,5 +24,12 @@ public class NodeController {
                 "/eureka", "");
     }
 
+    @PostMapping("/upload")
+    @ResponseStatus(HttpStatus.OK)
+    public String uploadFile(@RequestPart("chunk") byte[] chunk,
+                             @RequestPart("chunkId") String chunkId) throws Exception{
+        // TODO: Save the chunk and save the metadata in db
+        return "Chunk received successfully";
+    }
 
 }
