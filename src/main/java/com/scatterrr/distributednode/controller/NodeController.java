1543e639f4f80993da134e61b81b75ac672ff2b1
Akila-I <akila.99g@gmail.com>
2024-04-28 17:10:07 +0530

    Modify upload endpoint

--- src/main/java/com/scatterrr/distributednode/controller/NodeController.java
+++ src/main/java/com/scatterrr/distributednode/controller/NodeController.java
@@ -1,8 +1,11 @@
 package com.scatterrr.distributednode.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +17,7 @@ import java.nio.file.Paths;
 @RequestMapping("/api/node")
 public class NodeController {
 
+    private static final Logger log = LoggerFactory.getLogger(NodeController.class);
     @Value("${eureka.client.serviceUrl.defaultZone}")
     private String eurekaServerUrl;
 
@@ -26,10 +30,52 @@ public class NodeController {
 
     @PostMapping("/upload")
     @ResponseStatus(HttpStatus.OK)
-    public String uploadFile(@RequestPart("chunk") byte[] chunk,
-                             @RequestPart("chunkId") String chunkId) throws Exception{
-        // TODO: Save the chunk and save the metadata in db
-        return "Chunk received successfully";
+    public ResponseEntity<UploadResponse> uploadFile(
+            @RequestPart("chunk") byte[] chunk,
+            @RequestPart("chunkId") String chunkId,
+            @RequestPart("fileName") String fileName,
+            @RequestPart("merkleRootHash") String merkleRootHash,
+            @RequestPart("nextNode") String nextNode,
+            @RequestPart("prevHash") String prevHash
+            // more request body parts
+    ) throws Exception{
+        log.info("""
+                Received upload request with the data\s
+                filename {}\s
+                merkleRootHash {}\s
+                nextNode {}\s
+                prevHash {}""", fileName, merkleRootHash, nextNode, prevHash);
+
+        // TODO: Save the chunk in fileSystem
+
+        if (chunkSavedSuccessfully(chunk, chunkId)) {
+            // TODO: Save metadata of the chunk
+
+            // Return success message with hash
+            return ResponseEntity.ok(new UploadResponse(HttpStatus.OK.value(), "SAMPLE_HASH"));
+        } else {
+            // Return error message
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
+    // Check if chunk saving was successful
+    private boolean chunkSavedSuccessfully(byte[] chunk, String chunkId) {
+        // TODO
+        // Check whether the chunk with the chunkID is correctly stored in fileSystem
+        // DUMMY RETURN TRUE
+        return true;
+    }
+
+}
+
+// Class to hold upload response data
+class UploadResponse {
+    private final int statusCode;
+    private final String message;
+
+    public UploadResponse(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
 }
